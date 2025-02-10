3dc75221ba838a181ca8b257758921cf98f09bec
ryangregoire1999 <ryangregoire286@gmail.com>
2025-02-10 11:28:05 -0500

    Learned About Arrays

---
+++ Java Basics/Day 2/Arrays/Arrays/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String[] names = {"Ryan", "James", "G"};
+        int length = names.length;
+
+        for(int i = 0; i < length; i+= 1) {
+            System.out.println("Name: " + names[i]);
+        }
+
+    }
+}
+
